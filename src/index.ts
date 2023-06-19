d7678bc434c85f691d0f04cf1055e6b2c067d6b8
Darlan Alves <me@darlanalv.es>
2023-06-19 23:20:48 +0200

    fix: use await to chain promises

--- src/index.ts
+++ src/index.ts
@@ -18,36 +18,34 @@ export function fn(runOptions: RunOptions): (input: Blob) => Promise<Response> {
   const port = runOptions.local && runOptions.port ? ':' + runOptions.port : '';
   const searchParams = String(new URLSearchParams(Object.entries(Object(options))));
   const url = protocol + functionName + domain + port + '/' + action + '?' + searchParams;
-  let headers: Record<string, string>;
+  const headers: Record<string, string> = {};
 
   if (runOptions.credentials) {
     const token = btoa(JSON.stringify(runOptions.credentials));
-    headers = { Authorization: 'Bearer ' + token };
+    headers.authorization = 'Bearer ' + token;
   }
 
-  return (body: Blob) => fetch(url, { body, method: 'POST', ...(headers && { headers }) }).then(onFetch);
+  return async function (body: Blob) {
+    const response = await fetch(url, { body, method: 'POST', headers });
+
+    if (!response.ok) {
+      throw new Error(await response.text());
+    }
+
+    return response;
+  };
 }
 
-function onFetch(response: Response) {
-  if (!response.ok) {
-    return Promise.reject(response.text());
+export async function pipe(input: Response | Blob, ...streams: RunOptions[]): Promise<Response | Blob> {
+  let last: Response | Blob = input;
+
+  for (const step of streams) {
+    last = await fn(step)(await toBlob(last));
   }
 
-  return response;
+  return last;
 }
 
-export function pipe(input: Response | Blob | Promise<Response | Blob>, ...streams: RunOptions[]): Promise<Response> {
-  return streams.reduce((previous, next) => run(previous, next), toPromise(input));
-}
-
-function run(input: Promise<Response | Blob>, options: RunOptions): Promise<Response> {
-  return toPromise(input).then(toBlob).then(fn(options));
-}
-
-function toBlob(input: Blob | Response) {
-  return input instanceof Blob ? input : input.blob();
-}
-
-function toPromise(input) {
-  return input instanceof Promise ? input : Promise.resolve(input);
+function toBlob(input: Blob | Response): Promise<Blob> {
+  return input instanceof Blob ? Promise.resolve(input) : input.blob();
 }
